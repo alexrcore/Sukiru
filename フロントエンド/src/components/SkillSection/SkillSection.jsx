5f8b023bee358cbcc1a78a2ec65e945567dd1695
Meriem Soubih <soubih.meriem@gmail.com>
2025-06-18 00:30:57 +0100

    ⚡️: fixed scroll behavior of skill grid

--- フロントエンド/src/components/SkillSection/SkillSection.jsx
+++ フロントエンド/src/components/SkillSection/SkillSection.jsx
@@ -1,10 +1,13 @@
 import SearchBar from '../SearchBar/SearchBar'
 import SkillGrid from '../SkillGrid/SkillGrid'
+import './SkillSection.css'
 
 const SkillSection = ({ searchTerm, setSearchTerm, skills, selectedSkills, onToggleSkill }) => (
-  <section>
+  <section className="skill-section">
     <SearchBar value={searchTerm} onChange={setSearchTerm} />
-    <SkillGrid skills={skills} selectedSkills={selectedSkills} onToggleSkill={onToggleSkill} />
+    <div className="skill-grid-wrapper">
+      <SkillGrid skills={skills} selectedSkills={selectedSkills} onToggleSkill={onToggleSkill} />
+    </div>
   </section>
 )
 
