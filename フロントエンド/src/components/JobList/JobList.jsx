07ba08a564cc82d93a36cbf8100762256af04eff
Meriem Soubih <soubih.meriem@gmail.com>
2025-06-20 18:13:20 +0100

    ✨: add list of job cards as a component and style

---
+++ フロントエンド/src/components/JobList/JobList.jsx
@@ -0,0 +1,17 @@
+import JobCard from '../JobCard/JobCard.jsx'
+import './JobList.css'
+
+const JobList = () => {
+  return (
+    <div className="job-list">
+      <h2 className="sidebar-title">Your Roles</h2>
+      <div className="roles">
+        <JobCard title="Frontend Developer" percentage={75} missingSkills={['React', 'CSS']} />
+        <JobCard title="Backend Developer" percentage={50} missingSkills={['Node.js', 'Express']} />
+        <JobCard title="Full Stack Developer" percentage={60} missingSkills={['GraphQL', 'Docker']} />
+      </div>
+    </div>
+  )
+}
+
+export default JobList
