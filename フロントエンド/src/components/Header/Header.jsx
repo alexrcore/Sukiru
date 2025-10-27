3f0a44173b6c7b95ce8b00f2b797d26d3a390bd3
Alex Ryan <rayouvi.official@gmail.com>
2025-10-27 23:14:24 +0100

    🔧: use @ path alias

--- フロントエンド/src/components/Header/Header.jsx
+++ フロントエンド/src/components/Header/Header.jsx
@@ -3,7 +3,7 @@ import './Header.css'
 const Header = () => {
   return (
     <header className="header">
-      <h1 className="header-title">Sukiru</h1>
+      <h1 className="header-title">Sukiru - スキル</h1>
       <p className="header-subtitle">Discover how far you are from reaching your dream job!</p>
     </header>
   )
